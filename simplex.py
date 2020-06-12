955ef0d3571f2504d7ff128bccd52759a4e92907
Francisco Ventura <a173379@une.edu.mx>
2020-06-12 18:44:18 -0500

    Simplex de 2 Fases Listo

--- simplex.py
+++ simplex.py
@@ -21,21 +21,47 @@ def crear_tabla(cant_variables, cant_restricciones, lista_base):
         tabla_final.append(lista_base)
     return tabla_final
 
-z_function = True
-base_row_vals = ["s1", "s2", "s3"]
-base_col_vals = ["X1", "X2", "s1", "s2", "s3"]
+# z_function = True
+# base_row_vals = ["s1", "s2", "s3"]
+# base_col_vals = ["X1", "X2", "s1", "s2", "s3"]
 #                           CORECTO
-table_vals = [[15, 5, 1, 0, 0], [10, 6, 0, 1, 0], [8, 12, 0, 0, 1]]
-solution_vals = [300, 240, 450]
-z_vals = [-500, -300, 0, 0, 0, 0]
+# z_function_vals = [True, [500, 300]]
+# table_vals = [[15, 5, 1, 0, 0], [10, 6, 0, 1, 0], [8, 12, 0, 0, 1]]
+# solution_vals = [300, 240, 450]
+# last_row_vals = [-500, -300, 0, 0, 0, 0]
 # OUTPUT = ['x1: 15.00', 'x2: 15.00', 's3: 150.00', 'Z: 12,000.00']
 
 #                           CORECTO
+# z_function_vals = [True, [1, 2]]
 # table_vals = [[1, 3, 1, 0, 0], [2, 2, 0, 1, 0], [0, 1, 0, 0, 1]]
 # solution_vals = [200, 300, 60]
-# z_vals = [-1, -2, 0, 0, 0, 0]
-# OUTPUT = ['x1: 125.00', 's3: 35.00', 'x2: 25.00', 'Z: 175.00']
+# last_row_vals = [-1, -2, 0, 0, 0, 0]
+# OUTPUT = ['X1: 125.00', 'X2: 25.00', 's3: 35.00', 'Z: 175.00']
 
-t = Tablero(z_function, base_row_vals, base_col_vals, table_vals, solution_vals, z_vals)
+#                   Prueba Simplex 2 Fases
+#                           CORECTO
+# z_function = True
+# base_row_vals = ["r1", "r2", "r3"]
+# base_col_vals = ["X1", "X2", "e1", "e2", "e3", "r1", "r2", "r3"] 
+# z_function_vals = [False, [2000, 2000]]
+# table_vals = [[1, 2, -1, 0, 0, 1, 0, 0], [3, 2, 0, -1, 0, 0, 1, 0], [5, 2, 0, 0, -1, 0, 0, 1]]
+# solution_vals = [80, 160, 200]
+# last_row_vals = [0, 0, 0, 0, 0, -1, -1, -1, 0]
+# OUTPUT ESPERADO = x1 = 40, x2 = 20, e3 = 40, z = 120000
+# OUTPUT = ['X1: 40.00', 'X2: 20.00', 'e3: 40.00', 'Z: 120,000.00']
+
+#                   Prueba Simplex 2 Fases
+z_function = False
+base_row_vals = ["r1", "r2"]
+base_col_vals = ["X1", "X2", "e1", "e2", "r1", "r2"] 
+z_function_vals = [False, [30, 40]]
+table_vals =    [[20, 30, -1, 0, 1, 0], 
+                 [40, 30, 0, -1, 0, 1]]
+solution_vals = [3000, 4000]
+last_row_vals = [0, 0, 0, 0, -1, -1, 0]
+# OUTPUT ESPERADO = x1 = 50, x2 = 66.667, z = 4166.667
+# OUTPUT = ['X1: 50.00', 'X2: 66.67', 'Z: 4,166.67']
+
+t = Tablero(z_function, base_row_vals, base_col_vals, table_vals, solution_vals, last_row_vals, z_function_vals)
 result = t.solve_table()
 print(result)
--- tablero.py
+++ tablero.py
@@ -1,27 +1,33 @@
 from copy import deepcopy
 
+def truncate(n, decimals=0):
+    multiplier = 10 ** decimals
+    return int(n * multiplier) / multiplier
+
 class Tablero:
-    def __init__(self, mode, base_row, base_col, table, solution, z):
-        # True = Max
-        # False = Min
+    def __init__(self, mode, base_row, base_col, table, solution, last_row, z_function):
+        # True = Max, False = Min
         self.mode = mode
         self.base_row = base_row
         self.base_col = base_col
         self.table = table
         self.solution = solution
-        self.z = z
+        self.last_row = last_row
+        #  True = Max, False = Min
+        # [ True or False, [z_coefficients, ...] ]
+        self.z_function = z_function
 
     def found_x(self):
         # Copia de la ultima fila
-        copy_of_z = deepcopy(self.z[:])
+        copy_of_last_row = deepcopy(self.last_row[:])
         # Si es Max
         if self.mode:
             # Busca el menor en el último renglón
-            x = copy_of_z.index(min(copy_of_z[:-1]))
+            x = copy_of_last_row.index(min(copy_of_last_row[:-1]))
         # Si es Min
         else:
             # Busca el mayor en el último renglón
-            x = copy_of_z.index(max(copy_of_z[:-1]))
+            x = copy_of_last_row.index(max(copy_of_last_row[:-1]))
         return x
     
     def found_y(self, x):
@@ -58,11 +64,20 @@ class Tablero:
         y = self.found_y(x)
         return x,y
 
-    def z_has_negatives(self):
-        # Por cada valor en z, antes de la solución
-        for z in self.z[:-1]:
-            # Si z es negativo regresa cierto
-            if z < 0:
+    def last_row_has_negatives(self):
+        # Por cada valor en last_row, antes de la solución
+        for val in self.last_row[:-1]:
+            # Si last_row es negativo regresa cierto
+            if truncate(val, 4) < 0:
+                return True
+        # Regresa falso
+        return False
+
+    def last_row_has_positives(self):
+        # Por cada valor en last_row, antes de la solución
+        for val in self.last_row[:-1]:
+            # Si last_row es positivo regresa cierto
+            if truncate(val, 4) > 0:
                 return True
         # Regresa falso
         return False
@@ -70,6 +85,45 @@ class Tablero:
     def change_base(self, x, y):
         # Mover el nombre de columna en x a fila en y
         self.base_row[y] = self.base_col[x]
+
+    def base_row_has_r(self):
+        for base in self.base_row:
+            if "r" in base:
+                return True
+        return False
+
+    def is_basic_solution(self):
+        index_in_base_col = []
+        for base in self.base_row:
+            index_in_base_col.append(self.base_col.index(base))
+        for index in index_in_base_col:
+            if self.last_row[index] != 0:
+                return False
+        return True
+
+    def make_basic_solution(self):
+        index_of_row_col = []
+        index_of_negatives = [] 
+        for index, base in enumerate(self.base_row):
+            index_of_row_col.append((index, self.base_col.index(base)))
+        x_list = [x_index[1] for x_index in index_of_row_col]
+        for index, num in enumerate(self.last_row):
+            if index in x_list and num < 0:
+                index_of_negatives.append([y_x for y_x in index_of_row_col if y_x[1] == index][0])
+        copy_of_last_row = deepcopy(self.last_row)
+        for y_x_index in index_of_negatives:
+            for index in range(len(self.last_row) - 1):
+                self.last_row[index] += self.table[y_x_index[0]][index] *- copy_of_last_row[y_x_index[1]]
+            self.last_row[-1] += self.solution[y_x_index[0]] *- copy_of_last_row[y_x_index[1]]
+
+    def create_z_function(self):
+        self.last_row = []
+        last_row_len = len(self.base_col) + 1
+        self.mode = self.z_function[0]
+        for coefficient in self.z_function[1]:
+            self.last_row.append(-coefficient)
+        for i in range(last_row_len - len(self.last_row)):
+            self.last_row.append(0)
 
     def change_table_and_solution(self, x, y):
         # Crea una copia
@@ -91,24 +145,76 @@ class Tablero:
             if y_index != y:
                 if copy_of_table[y_index][x] != 0:
                     self.solution[y_index] = self.solution[y] *- copy_of_table[y_index][x] + copy_of_solution[y_index]
-        # Cambiar la fila z
-        copy_of_z = deepcopy(self.z)
-        if copy_of_z[x] != 0:
-            for x_index in range(len(copy_of_z) - 1):
-                self.z[x_index] = self.table[y][x_index] *- copy_of_z[x] + copy_of_z[x_index]
-            self.z[-1] = self.solution[y] *- copy_of_z[x] + copy_of_z[-1]
+        # Cambiar la fila last_row
+        copy_of_last_row = deepcopy(self.last_row)
+        if copy_of_last_row[x] != 0:
+            for x_index in range(len(copy_of_last_row) - 1):
+                self.last_row[x_index] = self.table[y][x_index] *- copy_of_last_row[x] + copy_of_last_row[x_index]
+            self.last_row[-1] = self.solution[y] *- copy_of_last_row[x] + copy_of_last_row[-1]
 
     def final_result(self):
         result = []
         for index in range(len(self.base_row)):
             result.append('{}: {:,.2f}'.format(self.base_row[index], self.solution[index]))
-        result.append('{}: {:,.2f}'.format('Z', self.z[-1]))
+        result.sort()
+        result.append('{}: {:,.2f}'.format('Z', self.last_row[-1]))
         return result
         
+    def solve_simplex(self):
+        if self.mode:
+            while self.last_row_has_negatives():
+                current_pivot = self.found_pivot()
+                self.change_base(*current_pivot)
+                self.change_table_and_solution(*current_pivot)
+        else:
+            while self.last_row_has_positives():
+                current_pivot = self.found_pivot()
+                self.change_base(*current_pivot)
+                self.change_table_and_solution(*current_pivot)
+
+    def remove_r(self):
+        temporal_table = []
+        temporal_row = []
+        temporal_solution = []
+        y_index_of_r = []
+        x_index_of_r = []
+        for index, base in enumerate(self.base_row):
+            if "r" in base:
+                y_index_of_r.append(index)
+        self.base_row = [row for row in self.base_row if "r" not in row]
+        for index, base in enumerate(self.base_col):
+            if "r" in base:
+                x_index_of_r.append(index)
+        self.base_col = [col for col in self.base_col if "r" not in col]
+        for y, row in enumerate(self.table):
+            if y not in y_index_of_r:
+                for x, col_val in enumerate(row):
+                    if x not in x_index_of_r:
+                        temporal_row.append(col_val)
+                temporal_table.append(temporal_row)
+                temporal_row = []
+        for y in range(len(self.solution)):
+            if y not in y_index_of_r:
+                temporal_solution.append(self.solution[y])
+        self.table = temporal_table
+        self.solution = temporal_solution
+
     def solve_table(self):
-        while self.z_has_negatives():
-            current_pivot = self.found_pivot()
-            self.change_base(*current_pivot)
-            self.change_table_and_solution(*current_pivot)
+        if self.base_row_has_r():
+            # Cambiar a Min
+            self.mode = False
+            if self.is_basic_solution() is not True:
+                self.make_basic_solution()
+            while self.last_row_has_positives():
+                current_pivot = self.found_pivot()
+                self.change_base(*current_pivot)
+                self.change_table_and_solution(*current_pivot)
+            self.remove_r()
+        self.create_z_function()
+        if self.is_basic_solution():
+            self.solve_simplex()
+        else:
+            self.make_basic_solution()
+            self.solve_simplex()
         result = self.final_result()
         return result
